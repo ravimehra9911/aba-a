a89c5095a77e2d54ace0890269d00c41b24c39a1
Ravi Mehra <ravimehra9911@gmail.com>
2023-04-24 16:10:46 +0530

    cart design

--- components/cart.js
+++ components/cart.js
@@ -1,28 +1,35 @@
+import { XMarkIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
 const Cart = ({ open, setOpen }) => {
   return (
     <div>
       {open && (
-        <div className="flex justify-center p-5 flex-col absolute bg-yellowColorDark top-0 right-0 h-screen">
+        <div className="flex px-[33px] w-1/2 p-5 flex-col absolute bg-yellowColor top-0 right-0 h-screen">
           <button onClick={() => setOpen(false)}>
-            <p>close</p>
+            <div className="flex justify-end">
+              <XMarkIcon className="h-10 w-10 stroke-[4]" />
+            </div>
           </button>
-          <p>Your Cart</p>
-          <div>
-            <p>
-              Building + Maintaining Rapport <span>1 General CEU</span>
+          <p className="font-spaceMonoRegular text-center  pt-5 p24">
+            Your Cart
+          </p>
+          <div className="flex justify-between pt-12 pb-6 relative">
+            <p className="font-interstateRegular ">
+              Building + Maintaining Rapport
+              <span className="font-interstateLight absolute top-[74px] left-0 ">
+                1 General CEU
+              </span>
             </p>
-            <p>$ 5.00</p>
+            <p className="font-interstateRegular">$ 5.00</p>
           </div>
-          <hr />
-          <div>
+
+          <div className="border-y-[1px] py-1 flex justify-between font-spaceMonoRegular text-[20px] mt-20 mb-10">
             <p>Price in USD</p>
             <p>$ 5.00</p>
           </div>
 
-          <hr />
-          <div>
+          <div className="flex justify-between">
             <button>EMPTY CART</button>
             <Link href="/checkoutAba">
               <button>CHECK OUT</button>
