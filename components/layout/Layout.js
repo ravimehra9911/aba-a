4e610b33c944f604b41e24511c53e83e5829f2c6
Ravi Mehra <ravimehra9911@gmail.com>
2023-05-02 17:12:42 +0530

    header

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Wrapper from './Wrapper';
-import Header from '../header/Headerr';
+import Header from '../header/Header';
 
 const Layout = ({ children }) => {
   return (
